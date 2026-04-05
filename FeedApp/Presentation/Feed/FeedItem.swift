139535afb687700d668fe6dc7052cd0a8d012a0a
PJ1nho <paveltihtey@gmail.com>
2026-04-05 16:37:04 +0300

    Add FeedItem model

---
+++ FeedApp/Presentation/Feed/FeedItem.swift
@@ -0,0 +1,18 @@
+//
+//  FeedItem.swift
+//  FeedApp
+//
+//  Created by Тихтей  Павел on 05.04.2026.
+//
+
+import Foundation
+
+struct FeedItem: Identifiable {
+    let id: Int
+    let name: String
+    let username: String
+    let postTitle: String
+    let postBody: String
+    let commentName: String
+    let commentBody: String
+}
