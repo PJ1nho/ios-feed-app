e31ef60496f76edd87f1f1ced698628c78736b67
PJ1nho <paveltihtey@gmail.com>
2026-04-05 14:36:45 +0300

    Create files for feed module

---
+++ FeedApp/Presentation/Feed/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  FeedApp
+//
+//  Created by Тихтей  Павел on 05.04.2026.
+//
+
+import SwiftUI
+import Combine
+
+final class FeedViewModel: ObservableObject {
+    
+}
