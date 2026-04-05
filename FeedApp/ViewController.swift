00eaa321c2b27ba5617c8bda4f336344d3def877
PJ1nho <paveltihtey@gmail.com>
2026-04-05 03:02:51 +0300

    Initial project setup with MVVM + Coordinator architecture

---
+++ FeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedApp
+//
+//  Created by Тихтей  Павел on 05.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
