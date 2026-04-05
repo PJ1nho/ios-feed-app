518631b28b6745fab4c668856dcc02e33481560e
PJ1nho <paveltihtey@gmail.com>
2026-04-05 15:50:02 +0300

    Add APIClient with posts, users and comments fetching

---
+++ FeedApp/Network/ApiClient.swift
@@ -0,0 +1,32 @@
+//
+//  ApiClient.swift
+//  FeedApp
+//
+//  Created by Тихтей  Павел on 05.04.2026.
+//
+
+import Foundation
+
+final class ApiClient {
+    static let usersUrl = "https://jsonplaceholder.typicode.com/users"
+    static let postsUrl = "https://jsonplaceholder.typicode.com/posts"
+    static let commentsUrl = "https://jsonplaceholder.typicode.com/comments"
+    
+    func fetchUsers() async throws -> [User] {
+        guard let url = URL(string: ApiClient.usersUrl) else { return [] }
+        let (users, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([User].self, from: users)
+    }
+    
+    func fetchPists() async throws -> [Post] {
+        guard let url = URL(string: ApiClient.postsUrl) else { return [] }
+        let (posts, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Post].self, from: posts)
+    }
+    
+    func fetchComments() async throws -> [Comment] {
+        guard let url = URL(string: ApiClient.commentsUrl) else { return [] }
+        let (comments, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Comment].self, from: comments)
+    }
+}
